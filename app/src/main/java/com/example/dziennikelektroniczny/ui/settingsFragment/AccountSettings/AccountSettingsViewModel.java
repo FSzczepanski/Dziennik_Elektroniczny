b914a1e18abafc137e3b3a1aad767bdd71dc4286
FSzczepanski <filip.szczepanskipv@gmail.com>
2020-12-28 15:07:23 +0100

    calculating average itc

--- app/src/main/java/com/example/dziennikelektroniczny/ui/settingsFragment/AccountSettings/AccountSettingsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.dziennikelektroniczny.ui.settingsFragment.AccountSettings;
-
-import androidx.lifecycle.ViewModel;
-
-public class AccountSettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
